a0812240e48206bf78b4541b7330a37ddc1e43e3
Arnaud KALCK <arnaud.kalck@d2si.io>
2020-06-09 15:35:34 +0200

    remove bad cred

--- terraform/vpc4packer/backend.tf
+++ terraform/vpc4packer/backend.tf
@@ -1,10 +1,9 @@
-
 terraform {
   backend "s3" {
-    bucket = "tfstate-gameday-chaos-2020-engieit268341914207"
-    key    = "app.tfstate"
-    region = "eu-west-1"
-    profile = "ice01"
+    bucket                  = "tfstate-gameday-chaos-2020-engieit268341914207"
+    key                     = "app.tfstate"
+    region                  = "eu-west-1"
+    profile                 = "ice01"
     shared_credentials_file = "../credentials"
   }
 }
